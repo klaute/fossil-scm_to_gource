a3a116578f06dc18a7c46f38ef2e57c5a6ca0b9c
Kai Lauterbach <klaute@web.de>
2015-04-03 23:59:24 +0200

    Comments added to test.sh.

--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+#
+# usage: ./test.sh <fossil repository name>
+#
+# author: Kai Lauterbach - klaute@gmail.com
+# date: 2015-04-03
+#
 fossil timeline -n 99999 -v -R $1 > timeline.txt
 
 python fossil_timeline_to_gource.py timeline.txt > timeline.log
